a1d1158917c69b37f8806317a44534c40d97f969
Christian Mansour <c.mansour@miami.edu>
2016-06-17 12:17:27 -0500

    working star wars api

--- star_wars.js
+++ star_wars.js
@@ -10,17 +10,21 @@ console.log("Welcome to the ultimate Star Wars experience! We are about to take 
 
  prompt.start();
   prompt.get(['characterName'], function(err, result) {
+  //   * CLI makes a simple call to *X* API using the user input
     console.log('You picked ' + result.characterName + '!');
     console.log('Here are some giphy\'s with that character.  Pick one!');
     request('http://api.giphy.com/v1/gifs/search?q=' + result.characterName.toLowerCase() + 's&api_key=dc6zaTOxFJmzC', function (error, response, body) {
         if (!error && response.statusCode == 200) {
-          response = JSON.parse(body);
-          console.log(response);
+          var response = JSON.parse(body);
+          var responseArr = response.data;
+          // console.log(responseArr[0].url);
+          open(responseArr[0].url);
         }
       });
   });
 
-//   * CLI makes a simple call to *X* API using the user input
+
+
 
     // Find Giphy URL for Star Wars giphys
 
